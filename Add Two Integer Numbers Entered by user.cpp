a82fbe709abd5bea458ca47ed83dc727fc3feb1e
SakshiKShanbhag <82280852+SakshiKShanbhag@users.noreply.github.com>
2021-10-26 23:05:41 +0530

    Add files via upload

---
+++ Add Two Integer Numbers Entered by user.cpp
@@ -0,0 +1,13 @@
+// Program to Add Two Integer Numbers Entered by user
+#include<iostream>
+using namespace std;
+int main(){
+
+int x,y,sum;
+cout<<"enter 2 integer nos :";
+cin>>x>>y;
+sum=x+y;
+cout<<"sum is = "<<sum;
+return 0;
+
+}
