0e8a4d0e894a440126ea04b27c94392fc52b88e8
SAKSHI K SHANBHAG <82280852+SakshiKShanbhag@users.noreply.github.com>
2021-11-07 19:36:29 +0530

    Add files via upload
    How to check if a given number is odd or even

---
+++ even odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+cin>>n;
+
+if (n%2==0)
+{
+    cout<< n <<"  is even number";
+}
+else
+{
+        cout<<n <<"  is an odd number";
+}
+return 0;
+}
+
