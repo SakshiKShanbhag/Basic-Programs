daba2266dadc07c1b337f03394f7d0a0bd0a37f0
SAKSHI K SHANBHAG <82280852+SakshiKShanbhag@users.noreply.github.com>
2021-11-15 23:48:24 +0530

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter the year:"<<endl;
+cin>>n;
+if(n%4==0)
+{
+    cout<<"its a leap year";
+}
+else cout<<"its not a leap year";
+return 0;
+}
