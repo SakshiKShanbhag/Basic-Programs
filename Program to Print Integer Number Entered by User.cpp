778ac956671c91c00c5eace2b40e1414dc32b60a
SAKSHI K SHANBHAG <82280852+SakshiKShanbhag@users.noreply.github.com>
2021-11-16 22:06:23 +0530

    Add files via upload

---
+++ Program to Print Integer Number Entered by User.cpp
@@ -0,0 +1,11 @@
+//Program to Print Integer Number Entered by User
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter the number:";
+cin>>n;
+cout<<"the number entered by user is : "<< n;
+
+return 0;
+}
